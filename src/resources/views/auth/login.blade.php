1be2b7ac05fdb7442b756092531e48e7aba29d44
Programmer <peshkov@mbober.ru>
2021-01-21 15:11:51 +0300

    fix login form

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -45,7 +45,11 @@
                             </div>
 
                             <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                <input class="form-check-input"
+                                       type="checkbox"
+                                       name="remember"
+                                       id="remember"
+                                        {{ old('remember') ? 'checked' : '' }}>
                                 <label class="form-check-label" for="remember">
                                     {{ __('Remember Me') }}
                                 </label>
