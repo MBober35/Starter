51d8931f3b24471c56617b56f253ec68cb106ee8
Programmer <peshkov@mbober.ru>
2021-03-02 16:46:10 +0300

    fix auth layout

--- src/resources/views/auth/passwords/email.blade.php
+++ src/resources/views/auth/passwords/email.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('content')
+@section('contents')
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8 col-lg-7 col-xl-6">
